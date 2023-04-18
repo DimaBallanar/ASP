a5093751281f79c76863b8ba86a4fb5dbfa0850d
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-19 01:09:23 +0300

    update work

---
+++ WebApplication1/Controllers/RestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RestController : ControllerBase
+    {
+        //https://localhost:7283/api/Rest/index/1
+        //[HttpGet("[action]/{id}")]
+        //public IActionResult Index(int id)
+        //{
+        //    return id % 2 == 0 ? Ok("число четное") : BadRequest();
+        //}
+
+        //https://localhost:7283/api/Rest/check-number/1
+        [HttpGet("check-number/{id}")]
+        public IActionResult Index(int id)
+        {
+            return id % 2 == 0 ? Ok("число четное") : BadRequest();
+        }
+
+        //https://localhost:7283/api/Rest/GetArray
+        //[HttpGet("[action]")]
+        //public IActionResult GetArray()
+        //{
+        //    return Ok(ArrayService.Array);
+        //}
+
+        [HttpGet("[action]")]
+        public string[] GetArray()
+        {
+            return ArrayService.Array;
+        }
+
+        [HttpGet("[action]/{index}")]
+        public string GetArray(int index)
+        {
+            return ArrayService.Array[index-1];
+        }
+
+
+    }
+
+}
---
+++ WebApplication1/Services/ArrayService.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Services
+{
+    public class ArrayService
+    {
+      public static string[] Array = {"asd","gg","12341f","vvvv","rttt","p44","9dsf","000ddfrr"};
+
+    }
+}
