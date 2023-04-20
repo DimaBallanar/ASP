35a44ff14cc397ba784a910fce2b6cceb09ff6e1
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-20 22:22:35 +0300

    Repositories/LanguageRepositories.cs

---
+++ WebApplication1/Repositories/LanguageRepositories.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Text;
+using System.Text.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repositories
+{
+    public class LanguageRepositories
+    {
+        static string Path = "D:\\ДЗ С#\\hschool\\hschool_beggining_csh\\ASP\\WebApplication1\\bin\\Debug\\net6.0\\languageBD.txt";
+        public async Task<List<Language>> GetAll()
+        {
+            try
+            {
+                using StreamReader sr2 = new StreamReader(Path, Encoding.UTF8);
+                string result = await sr2.ReadToEndAsync();
+                return JsonSerializer.Deserialize<List<Language>>(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task<Language> GetById(string id)
+        {
+            try
+            {
+                var languages = await GetAll();
+                return languages.FirstOrDefault(x => x.Id.ToLower()==id.ToLower());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task WriteNew(List<Language> language)
+        {
+            try
+            {
+                using StreamWriter sw = new StreamWriter(Path);
+                string text=JsonSerializer.Serialize(language);
+                sw.WriteLine(text);
+                sw.Close();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
