f4f2ab19c5987c40741be66c16805bb9ef2672fc
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-20 22:22:10 +0300

    Models/Language.cs

---
+++ WebApplication1/Models/Language.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication1.Models
+{
+    public class Language
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+        [JsonPropertyName("priority")]
+        public int Priority { get; set; }
+
+    }
+}
