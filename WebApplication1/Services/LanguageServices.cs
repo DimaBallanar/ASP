140c651cd62b93c5f4eedfe9a789760b5a818b4b
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-20 22:22:49 +0300

    Services/LanguageServices.cs

---
+++ WebApplication1/Services/LanguageServices.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Services
+{
+    public class LanguageServices
+    {        
+        public async Task<List<Language>> GetAll()
+        {
+            return await new LanguageRepositories().GetAll();
+        }
+
+        public async Task<Language> GetById(string id)
+        {
+            return await new LanguageRepositories().GetById(id);
+        }
+
+        public async Task Create( Language lg)
+        {
+            var list = await new LanguageRepositories().GetAll();
+            list.FirstOrDefault(lg=> lg.Id == lg.Id);
+            if()
+        }
+
+    }
+}
