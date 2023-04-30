2c41b5b31cde7b6052bc8d6ea855c76806c57d0e
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-30 17:32:37 +0300

    AppSettingsHelper.cs

---
+++ WebApplication1/Services/AppSettingsHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Services
+{
+    public static class AppSettingsHelper
+    {
+        private static string _connectionString;
+        public static string ConnectionString => _connectionString ??= GetBuilder().GetConnectionString("MyDatabase");
+        //private static string _pathFolder;
+        //public static string PathFolder => _pathFolder ??= GetBuilder().GetValue<string>("PathFolder");
+
+        private static IConfigurationRoot GetBuilder()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("AppSettings.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
+    }
+}
